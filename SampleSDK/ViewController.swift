c7ee6ecc4f4130e0c564e4d929a9e8084348bb58
Muhammad Amir Pervaiz <amir.pervaiz@quantuminventions.com>
2020-08-20 18:44:45 +0800

    init

---
+++ SampleSDK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleSDK
+//
+//  Created by Amir Pervaiz on 20/8/20.
+//  Copyright © 2020 Amir Pervaiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
